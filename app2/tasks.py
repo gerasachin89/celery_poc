ba7979f2d2cd98783ce5c3a04416a787aa2754fb
Sachin Gera <sachin.gera@verificient.com>
2018-03-07 21:41:52 +0530

    task run after worker crash using ask_late

--- app2/tasks.py
+++ app2/tasks.py
@@ -1,13 +1,26 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from celery.task import task
+import time
 
 #@task(queue = 'feeds')
 @shared_task
 def add(x, y):
+    time.sleep(30)
     return x + y
 
 
-@shared_task
+@task(acks_late=True, reject_on_worker_lost=True)
 def mexitul(x, y):
+    time.sleep(30)
     return x * y
+
+@task(bind=True, acks_late=True)
+def add1(self, x, y):
+    try:
+        return x * y *z
+    except Exception as e:
+        self.update_state(state='FAILURE', meta={'exc': e})
+        self.retry(countdown=2, exc=e)
+        #self.retry(args=[5, 7], exc=e, countdown=30)
+    return x + y
